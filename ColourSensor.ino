fd236a4a7cbccb00029728d1087101071bbc2bf5
kirstypurden <kirsty.purden.2015@uni.strath.ac.uk>
2020-05-01 09:27:58 +0100

    Added some comments

--- ColourSensor.ino
+++ ColourSensor.ino
@@ -3,7 +3,8 @@
 int taosOutPin = 9;
 int LED = 7;
 int input = 0;
-int before, after = 0, 0;
+int before = 0;
+int after =  0;
 
 void setup() 
 {
@@ -14,15 +15,16 @@ void setup()
   delay(100); //wait for it to set up (100 ms)
 }
 
-Serial.print("Pressl n to take no light reading."); //Print no light reading message
+
 void loop() //loop forever
 { 
-
-  if (Serial.available() > 0) 
-  {
+  Serial.print("Pressl n to take no light reading."); //Print no light reading message
+  if (Serial.available() > 0) // used for testing so we had time to turn the light on and off. Entered n when we were ready to take a reading.
+  { //in final system the arudino would turn some LEDs on and the user interaction bits could be removed
       input = Serial.read();
 
       //Take no light reading
+      //(take a reading when we're not shining the light on it)
       if(input == 110) //n
       {
           before = detectColor(taosOutPin);//see what colour we're detecting
@@ -30,14 +32,15 @@ void loop() //loop forever
       }
 
       //Take light reading and display result
-      if(input==108) //input=l
+      if(input==108) //input=l - entered l when we were ready to take a reading with the light on
       {
         after = detectColor(taosOutPin);
   
         int difference, percentage_drop;
-        difference = before - after;
-        percentage_drop = difference/before;
+        difference = before - after;//compare the value before and after the light was shone
+        percentage_drop = difference/before; //calculate the percentage drop - this is different for black and white so should tells us which it is
 
+        //display these values on the serial monitor
         Serial.print("Before: ");
         Serial.print(before);
         Serial.print("\n");
@@ -49,16 +52,15 @@ void loop() //loop forever
         Serial.print("\n");
   
 
-        if(percentage_drop<0.15)
+        if(percentage_drop<0.15) //if it drops only a little bit, it's most likely the black skittle
         {
           Serial.print("Skittle is black");
         }
-        if(percentage_drop>30)
+        if(percentage_drop>30)//if it drops by a much higher amount, it's most likely white
         {
           Serial.print("Skittle is white");
         }
-        Serial.print("Pressl n to take no light reading."); //Print no light reading message
-        //delay(1000); //wait 1000 ms before reading again
+
       }
   }
 }
